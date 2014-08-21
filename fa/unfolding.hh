64959ee1ac95c222d910c051f3bcde4a3eadf91b
Martin Hruska <hruskamartin25@gmail.com>
2014-08-21 15:07:21 +0200

    solved with unfolding

--- fa/unfolding.hh
+++ fa/unfolding.hh
@@ -36,147 +36,25 @@ class Unfolding
 	FAE& fae;
 
 protected:
-
 	void boxMerge(
 		TreeAut&                       dst,
 		const TreeAut&                 src,
 		const TreeAut&                 boxRoot,
 		const Box*                     box,
-		const std::vector<size_t>&     rootIndex)
-	{
-		TreeAut tmp(this->fae.ta), tmp2(this->fae.ta);
-//		this->fae.boxMan->adjustLeaves(tmp2, boxRoot);
-		this->fae.relabelReferences(tmp, boxRoot, rootIndex);
-		this->fae.unique(tmp2, tmp);
-		src.copyNotAcceptingTransitions(dst, src);
-		tmp2.copyNotAcceptingTransitions(dst, tmp2);
-		dst.addFinalStates(tmp2.getFinalStates());
+		const std::vector<size_t>&     rootIndex);
 
-		for (size_t state : src.getFinalStates())
-		{
-			for (auto i = src.begin(state); i != src.end(state, i); ++i)
-			{
-				std::vector<size_t> lhs;
-				std::vector<const AbstractBox*> label;
-				size_t lhsOffset = 0;
-				if (box) {
-					bool found = false;
-					for (const AbstractBox* aBox : TreeAut::GetSymbol(*i)->getNode()) {
-						if (!aBox->isStructural()) {
-							label.push_back(aBox);
-							continue;
-						}
-						const StructuralBox* b = static_cast<const StructuralBox*>(aBox);
-						if (b != static_cast<const StructuralBox*>(box)) {
-							// this box is not interesting
-							for (size_t k = 0; k < b->getArity(); ++k, ++lhsOffset)
-								lhs.push_back((*i).GetNthChildren(lhsOffset));
-							label.push_back(b);
-							continue;
-						}
-						lhsOffset += box->getArity();
-
-						if (found)
-							assert(false);
-
-						found = true;
-					}
-
-					if (!found)
-						assert(false);
-
-				} else {
-					lhs = (*i).GetChildren();
-					label = TreeAut::GetSymbol(*i)->getNode();
-				}
-				for (auto j = tmp2.accBegin(); j != tmp2.accEnd(j); ++j) {
-					std::vector<size_t> lhs2 = lhs;
-					std::vector<const AbstractBox*> label2 = label;
-					lhs2.insert(lhs2.end(), (*j).GetChildren().begin(), (*j).GetChildren().end());
-					label2.insert(label2.end(),
-                            TreeAut::GetSymbol(*j)->getNode().begin(),
-                            TreeAut::GetSymbol(*j)->getNode().end());
-					FA::reorderBoxes(label2, lhs2);
-					dst.addTransition(lhs2, this->fae.boxMan->lookupLabel(label2), (*j).GetParent());
-				}
-			}
-		}
-	}
+private:
+    void getChildrenAndLabelFromBox(
+		const Box*                         box,
+        const TreeAut::Transition&         transition,
+        std::vector<size_t>&               children,
+        std::vector<const AbstractBox*>&   label);
 
 public:
-
-	void unfoldBox(size_t root, const Box* box)
-	{
-		assert(root < this->fae.getRootCount());
-		assert(nullptr != this->fae.getRoot(root));
-		assert(nullptr != box);
-
-		const TreeAut::Transition& t = 
-            this->fae.getRoot(root)->getAcceptingTransition();
-
-		size_t lhsOffset = 0;
-		std::vector<size_t> index = { root };
-
-		for (const AbstractBox* aBox : TreeAut::GetSymbol(t)->getNode())
-		{
-			if (static_cast<const AbstractBox*>(box) != aBox)
-			{
-				lhsOffset += aBox->getArity();
-
-				continue;
-			}
-
-			for (size_t j = 0; j < box->getArity(); ++j)
-			{
-				const Data& data = this->fae.getData(t.GetNthChildren(lhsOffset + j));
-
-				if (data.isUndef())
-					index.push_back(static_cast<size_t>(-1));
-				else
-					index.push_back(data.d_ref.root);
-			}
-
-			break;
-		}
-
-		auto ta = std::shared_ptr<TreeAut>(this->fae.allocTA());
-
-		this->boxMerge(*ta, *this->fae.getRoot(root), *box->getOutput(), box, index);
-
-		this->fae.setRoot(root, ta);
-		this->fae.connectionGraph.invalidate(root);
-
-		if (!box->getInput())
-			return;
-
-		assert(box->getInputIndex() < index.size());
-
-		size_t aux = index[box->getInputIndex() + 1];
-
-		assert(aux != static_cast<size_t>(-1));
-		assert(aux < this->fae.getRootCount());
-
-		TreeAut tmp(this->fae.ta);
-
-		this->fae.getRoot(aux)->unfoldAtRoot(tmp, this->fae.freshState());
-		this->fae.setRoot(aux, std::shared_ptr<TreeAut>(this->fae.allocTA()));
-
-		this->boxMerge(*this->fae.getRoot(aux), tmp, *box->getInput(), nullptr, index);
-
-		this->fae.connectionGraph.invalidate(aux);
-
-//		this->fae.updateConnectionGraph();
-	}
-
-	void unfoldBoxes(size_t root, const std::set<const Box*>& boxes) {
-		for (std::set<const Box*>::const_iterator i = boxes.begin(); i != boxes.end(); ++i)
-			this->unfoldBox(root, *i);
-	}
-
-public:
-
 	Unfolding(FAE& fae) : fae(fae) {}
 
+	void unfoldBox(size_t root, const Box* box);
+	void unfoldBoxes(size_t root, const std::set<const Box*>& boxes);
 };
 
 #endif
