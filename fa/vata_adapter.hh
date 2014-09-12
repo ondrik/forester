c6c8b50dad7a2cf32229d3d07a8d949b54fd749f
Martin Hruska <hruskamartin25@gmail.com>
2014-09-12 12:36:55 +0200

    added documentation

--- fa/vata_adapter.hh
+++ fa/vata_adapter.hh
@@ -58,6 +58,7 @@ private: // private data types
         }
     };
 
+
     /**
      * @brief functor copies only not accepting transitions
      *
@@ -77,14 +78,18 @@ private: // private data types
         }
     };
 
+
 private: // private constants
     const int cEmptyRootTransIndex = 0; // index of root transition
+
 
 private: // data members
     TreeAut vataAut_;
 
+
 private: // private methods
 		VATAAdapter(TreeAut aut);
+
 
 public: // public methods
 		VATAAdapter();
@@ -102,6 +107,7 @@ public: // public methods
     static VATAAdapter createTAWithSameTransitions(
 				const VATAAdapter&         ta);
 
+
     /**
      * @brief Function allocates a new TA with same transitions as the given one
      *
@@ -112,6 +118,7 @@ public: // public methods
      */
     static VATAAdapter* allocateTAWithSameTransitions(
 				const VATAAdapter&         ta);
+
     
     /**
      * @brief Function could create TA with same transitions and final states
@@ -126,6 +133,7 @@ public: // public methods
     static VATAAdapter createTAWithSameFinalStates(
 				const VATAAdapter&         ta,
         bool                       copyFinalStates=true);
+
 
     /**
      * @brief Function could allocate TA with same transitions and final states
@@ -142,6 +150,7 @@ public: // public methods
 				const VATAAdapter&         ta,
         bool                       copyFinalStates=true);
 
+
     /**
      * @brief Returns iterator over transitions
      *
@@ -152,12 +161,14 @@ public: // public methods
      */
 		iterator begin() const;
 
+
     /**
      * @brief Returns iterator pointing to the end of transitions
      *
      * @return End of transitions of @vataAut
      */
 		iterator end() const;
+
 
     /**
      * @brief Returns iterator over transitions with parent @rhs
@@ -171,6 +182,7 @@ public: // public methods
     DownAccessor::Iterator begin(const size_t parent) const;
     DownAccessor::Iterator end(const size_t parent) const;
 
+
     /**
      * @brief Returns iterator over a set of the accepting transitions
      *
@@ -178,6 +190,7 @@ public: // public methods
      */
     AcceptTrans::Iterator accBegin() const;
 		AcceptTrans::Iterator accEnd() const;
+
 
     /**
      * @brief Copies TreeAut of @rhs to this @vataAut_
@@ -188,6 +201,7 @@ public: // public methods
      * @return Reference to this object
      */
     VATAAdapter& operator=(const VATAAdapter& rhs);
+
 
     /**
      * @brief Adds a new transition to @vataAut_
@@ -203,6 +217,7 @@ public: // public methods
 				const SymbolType&                   symbol,
 				const size_t                        parent);
 
+
     /**
      * @brief Adds a new @transition to @vataAut_
      *
@@ -213,6 +228,7 @@ public: // public methods
      * @param[in] parent A parent state of the new transition
      */
     void addTransition(const Transition& transition);
+
 
     /**
      * @brief Returns transition with the same parameters
@@ -231,6 +247,7 @@ public: // public methods
 				const SymbolType&                   symbol,
 				const size_t                        parent);
 
+
     /**
      * @brief Returns label_type structure for a given transition
      *
@@ -241,12 +258,14 @@ public: // public methods
      */
 		static const label_type GetSymbol(const Transition& t);
 
+
     /**
      * @brief Add one final state to the set of final states of @treeAut_
      *
      * @param[in] state State which is being added to the final states set
      */
     void addFinalState(const size_t state);
+
     
     /**
      * @brief Add a set of states to the set of final states of @treeAut_
@@ -257,6 +276,7 @@ public: // public methods
      * @param[in] states States to be set as the final ones
      */
 		void addFinalStates(const std::set<size_t>& states);
+
     
     /**
      * @brief Add a set of states to the set of final states of @treeAut_
@@ -268,12 +288,14 @@ public: // public methods
      */
 		void addFinalStates(const std::unordered_set<size_t>& states);
 
+
     /**
      * @brief Check whether a given state is final one
      *
      * @return Returns true if a given state is final one
      */
     bool isFinalState(size_t state) const;
+
     
     /**
      * @brief Returns a set of the final states of @vataAut_
@@ -281,6 +303,7 @@ public: // public methods
      * @return A set of final states
      */
 		const std::unordered_set<size_t>& getFinalStates() const;
+
     
     /**
      * @brief Returns one final state
@@ -293,6 +316,7 @@ public: // public methods
      */
 		size_t getFinalState() const;
 
+
     /**
      * @brief Returns one accepting transition.
      *
@@ -300,6 +324,7 @@ public: // public methods
      * A precondition is that there exists just one accepting transition.
      */
     const Transition getAcceptingTransition() const;
+
 
     /**
      * @brief Remove unreachable states from @vataAut_ and saves resulting TA to @dst.
@@ -312,6 +337,7 @@ public: // public methods
      */
     VATAAdapter& unreachableFree(VATAAdapter& dst) const;
 
+
     /**
      * @brief Remove useless states from @vataAut_ and saves resulting TA to @dst.
      *
@@ -322,6 +348,7 @@ public: // public methods
      * @return TA equivalent to @vataAut_ without useless states
      */
 		VATAAdapter& uselessAndUnreachableFree(VATAAdapter& dst) const;
+
     
     /**
      * @brief Remove unreachable and useless states from @vataAut_ and saves resulting TA to @dst.
@@ -333,6 +360,7 @@ public: // public methods
      * @return TA equivalent to @vataAut_ without unreachable states
      */
     VATAAdapter& minimized(VATAAdapter& dst) const;
+
 
     /**
      * @brief Union of two automata with disjoint states sets
@@ -350,6 +378,7 @@ public: // public methods
 				const VATAAdapter&                src,
 				bool                              addFinalStates = true);
 
+
     /**
 		 * @brief Returns true if there are no transitions in TA
      * Return true if there are no transitions in automata
@@ -357,14 +386,21 @@ public: // public methods
      */
     bool areTransitionsEmpty();
 
+
 		/**
 		 * @brief Copies all transtition to @dst
 		 * Function copies all transitions from @vataAut_ to
-		 * @dst and returns newly filled automata
+		 * @param[in] dst and returns newly filled automata
 		 * @return TA with copied transitions 
 		 */
 		VATAAdapter& copyTransitions(VATAAdapter& dst) const;
 
+
+		/**
+		 * @brief Implementation of the visitor patter
+		 * Implements visitor pattern over this TA
+		 * @param[in] Visitor object
+		 */ 
 		template <class TVisitor>
 		void accept(TVisitor& visitor) const
 		{
@@ -372,36 +408,117 @@ public: // public methods
 				visitor(*this);
 		}
 
+
+		/**
+		 * @brief Copy all transitions not leading to a final state.
+		 * Function copies all transitions of this TA, which cannot
+		 * move to a final state of @ta, to @dst.
+		 * @param[out] dst Target TA where transitions will be copied
+		 * @param[in] ta TA which final states are taken into consideration
+		 */
 		VATAAdapter& copyNotAcceptingTransitions(
             VATAAdapter& dst,
             const VATAAdapter& ta) const;
 
+		
+		/**
+		 * @brief Removes all transitions and final states.
+		 * Function removes all transitions and final states of this TA.
+		 */
     void clear();
 
+
+		/**
+		 * @brief Checks whether language of @a is subset or equal to a language of @b
+		 * Function checks whether language of @a is subset or equal to a language of @b
+		 * @param[in] a Smaller TA
+		 * @param[in] b Bigger TA
+		 * @return True when inclusion holds otherwise false
+		 */
 		static bool subseteq(
 				const VATAAdapter& a,
 				const VATAAdapter& b);
 
+    /**
+		 * @brief Copies to @dst all transitions whom parent is a final state
+		 * Function copies all transition of this TA to @dst if a parent of 
+		 * a transition is final states of this TA. If @registerFinalState
+		 * is true, it also sets @newState as a final state of @dst
+		 * @param[out] dst TA where transition will be store
+		 * @param[in]  newState State to be set as a final one.
+		 * @param[in]  registerFinalState Determines setting a @newState as a final one.
+		 */
     VATAAdapter& unfoldAtRoot(
 				VATAAdapter&             dst,
 				const size_t             newState,
 				bool                     registerFinalState = true) const;
 
+
+		/**
+		 * @brief Copies all transitions and renames final states
+		 * Firstly function copies all transitions of this TA to @dst.
+		 * Then function gradually renames all final states of this TA
+		 * and adds transition related to a final state again but now with
+		 * the renamed parent. If @registerFinalState if true it also set
+		 * renamed states as final ones.
+     * @param[out] dst TA where transition will be store
+     * @param[in]  states States translator.
+     * @param[in]  registerFinalState Determines whether the renamed state are set as finale.
+     */
 		VATAAdapter& unfoldAtRoot(
 				VATAAdapter&                                  dst,
 				const std::unordered_map<size_t, size_t>&     states,
 				bool                                          registerFinalState = true) const;
 
+
+		/**
+		 * @brief Builds index of all used states in this TA
+     * Builds index of all used states in this TA. Index maps each state
+		 * to a number 0...n-1, where n is number of used states in TA.
+     * @param[in]  index Built index.
+     */
 		void buildStateIndex(Index<size_t>& index) const;
-		
+
+		/**
+		 * @brief Returns set of all used states in TA
+		 * Returns set of all used states in TA.
+		 * @return Set of all used states
+		 */
 		std::unordered_set<size_t> getUsedStates() const;
   
+		
+		/**
+		 * @brief Returns iterator over accepting transitions
+		 * Returns iterator over accepting transitions.
+		 * There has to be exactly one final state and the state
+		 * has to be @cEmptyRootsTransitionIndex
+		 * @return Iterator over accpeting transitions
+		 */
 		TreeAut::AcceptTrans getEmptyRootTransitions() const;
 
+
+		/**
+		 * @brief Copies all transition accessible from @rootState
+		 * Copies all transitions of @src reachable from @rootState to this TA
+     * @param[in]  src TA with transitions to be copied to this TA.
+     * @param[in]  rootState root state from which TA will be explored
+		 */
     void copyReachableTransitionsFromRoot(
             const VATAAdapter&        src,
-            const size_t&    rootState);
+            const size_t&             rootState);
 
+
+		/**
+		 * @brief Renames states in @src automata and stores them to @dst
+		 * Renames all states in @src automata using @funcRename function
+		 * and stores them to @dst. If @addFinalStates is true, also the
+		 * final states will be stored to @dst.
+     * @param[out]  dst TA where transitions will be stored
+     * @param[in]  src TA with transitions to be copied to this @dst.
+     * @param[in]  funcName Renaming function.
+     * @param[in]  addFinalStates Determines whether final states should be copied.
+		 * @return @dst automata after copying is finished. 
+		 */
     template <class F>
     static VATAAdapter& rename(
             VATAAdapter&                   dst,
@@ -434,7 +551,16 @@ public: // public methods
         return dst;
     }
 
-    // currently erases '1' from the relation
+
+		/**
+		 * @brief Computes height abstraction over this TA.
+		 * Function computer height abstraction over this TA and stores
+		 * the result to @result relation.		
+     * @param[out] result Relation with height abstraction.
+		 * @param[in]  height Number of iterations.
+     * @param[in]  f      Function for comparing the labels of the transitions.
+     * @param[in]  g      Function for checking signature of states.
+		 */
 		template <class F, class G>
 		void heightAbstraction(
 				std::unordered_map<size_t,size_t>&         result,
@@ -446,7 +572,14 @@ public: // public methods
      		VATAAbstraction::heightAbstraction(vataAut_, result, height, f, g);
 		}
 
-		// collapses states according to a given relation
+
+		/**
+		 * @brief Collapses states according to a given relation.
+		 * Function collapses states of this TA according to @rel and
+		 * stores them to @dst.
+     * @param[out] dst    TA after collaption of states.
+		 * @param[in]  rel    Input relation of collaption.
+		 */
 		VATAAdapter& collapsed(
 				VATAAdapter&                                 dst,
 				const std::unordered_map<size_t, size_t>&    rel) const;
