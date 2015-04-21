1231dd2aac36a5518b8e3c40f755c4015714ff62
Martin Hruska <hruskamartin25@gmail.com>
2015-04-21 17:00:28 +0200

    predicate abstraction

--- fa/abstraction.cc
+++ fa/abstraction.cc
@@ -1,6 +1,9 @@
 #include "abstraction.hh"
 
 #include "streams.hh"
+#include "vata_adapter.hh"
+
+#include <vata/aut_base.hh>
 
 #include <vector>
 
@@ -24,10 +27,25 @@ void Abstraction::predicateAbstraction(
 	// TODO: use boost::dynamic_bitset
 	std::vector<std::vector<bool>> rel;
 
-	if (!predicates.empty())
+	for (const auto& predicate : predicates)
 	{
 		FA_NOTE("Predicate: " << *predicates.back());
+		assert(predicates.back()->getRootCount() == this->fae_.getRootCount());
 
+		VATA::AutBase::ProductTranslMap translMap;
+		for (size_t root = 0; root < this->fae_.getRootCount(); ++root)
+		{
+			VATAAdapter::intersectionBU(*this->fae_.getRoot(root), *(predicate->getRoot(root)), &translMap);
+
+		}
+
+		std::vector<std::set<size_t>> matchWith(numStates, std::set<size_t>());
+		for (const auto& matchPair : translMap)
+		{
+			matchWith[faeStateIndex[matchPair.first.first]].insert(matchPair.first.second);
+		}
+		
+		/*
 		std::set<std::pair<size_t, size_t>> product;
 		FAE::makeProduct(fae_, *predicates.back(), product);
 
@@ -37,6 +55,7 @@ void Abstraction::predicateAbstraction(
 		{
 			matchWith[faeStateIndex[statePair.first]].insert(statePair.second);
 		}
+		*/
 
 		std::ostringstream oss;
 		for (size_t i = 0; i < matchWith.size(); ++i)
@@ -44,9 +63,8 @@ void Abstraction::predicateAbstraction(
 			oss << ", " << i << " -> ";
 			utils::printCont(oss, matchWith[i]);
 		}
-
 		FA_NOTE("matchWith: " << oss.str());
-
+		
 		// create the relation
 		rel.assign(numStates, std::vector<bool>(numStates, false));
 		for (size_t i = 0; i < numStates; ++i)
@@ -63,7 +81,7 @@ void Abstraction::predicateAbstraction(
 			}
 		}
 	}
-	else
+	if (predicates.empty()) //else
 	{
 		// create universal relation
 		rel.assign(numStates, std::vector<bool>(numStates, true));
@@ -77,7 +95,7 @@ void Abstraction::predicateAbstraction(
 		ConnectionGraph::StateToCutpointSignatureMap stateMap;
 		ConnectionGraph::computeSignatures(stateMap, *fae_.getRoot(i));
 		for (const auto j : faeStateIndex)
-		{	// go through the matrix
+		{ // go through the matrix
 			for (const auto k : faeStateIndex)
 			{
 				if (k == j)
@@ -131,13 +149,38 @@ void Abstraction::predicateAbstraction(
 	FA_NOTE("Index: " << ossInd.str());
 
 	// TODO: label states of fae_ by states of predicate
+	std::unordered_map<size_t, size_t> relCom;
+	for (size_t state = 0; state < numStates; ++state)
+	{
+		relCom[state] = state;
+	}
+
+	for (const auto& item1 : faeStateIndex)
+	{
+		for (const auto& item2 : faeStateIndex)
+		{
+			if (rel[item1.second][item2.second])
+			{
+				relCom[item2.first] = item1.first;
+			}
+		}
+	}
+
+	for (const auto& item : faeStateIndex)
+	{
+		if (relCom.count(item.second) == 0)
+		{
+			relCom[item.second] = item.second;
+		}
+	}
 
 	for (size_t i = 0; i < fae_.getRootCount(); ++i)
 	{
-		TreeAut ta = fae_.createTAWithSameBackend();
-		//fae_.getRoot(i)->collapsed(ta, rel, faeStateIndex);
-		fae_.setRoot(i, std::shared_ptr<TreeAut>(fae_.allocTA()));
-		ta.uselessAndUnreachableFree(*fae_.getRoot(i));
+		TreeAut* ta = fae_.allocTA();
+		fae_.getRoot(i)->collapsed(*ta, relCom);
+		FA_NOTE("NEW TA " << *ta);
+		fae_.setRoot(i, std::shared_ptr<TreeAut>(ta));
+		//ta.uselessAndUnreachableFree(*fae_.getRoot(i));
 	}
 
 	FA_NOTE("Predicate abstraction output: " << fae_);
